591318685b428eda70cdd43e3b94aa9841f29649
Arafin Mridul <mridularafin@gmail.com>
2024-02-26 00:47:40 +0600

    bg n modifica

---
+++ directives.ts
@@ -0,0 +1,7 @@
+// Installation
+// npx create-expo-app ./
+// npx expo install react-dom react-native-web @expo/metro-runtime
+
+// Styling
+// In React Native, styling is done using JavaScript as compared to the web, where CSS is used
+// Most of the React Native core components accept a style prop that accepts a JavaScript object as its value
